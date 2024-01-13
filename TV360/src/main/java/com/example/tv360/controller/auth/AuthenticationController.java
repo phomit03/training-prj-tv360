70d2c6d1e574ed12e4623394bb9eedae03234f8f
lcisme <92872523+lcisme@users.noreply.github.com>
2024-01-13 17:46:46 +0700

    fix mediadetails and fix bug /login, phan quyen mediadetails

--- TV360/src/main/java/com/example/tv360/controller/auth/AuthenticationController.java
+++ TV360/src/main/java/com/example/tv360/controller/auth/AuthenticationController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class AuthenticationController {
 	private UserService userService;
@@ -25,10 +27,16 @@ public class AuthenticationController {
     }
 
 	@GetMapping("/login")
-	public String login() {
+	public String login(HttpSession session) {
+		User loggedInUser = (User) session.getAttribute("loggedInUser");
+		if (loggedInUser != null) {
+			// Nếu đã đăng nhập, chuyển hướng đến trang home
+			return "redirect:/home";
+		}
+
+		// Nếu chưa đăng nhập, hiển thị trang đăng nhập
 		return "auth_login";
 	}
-
 	@GetMapping("/registration")
 	public String showRegistrationForm() {
 		return "auth_register";
